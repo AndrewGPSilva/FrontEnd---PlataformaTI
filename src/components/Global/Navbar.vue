e943abd726cee4171ec5f012a86dc682c105951a
Andrew Gomes <andrewgomes1328@gmail.com>
2023-10-29 16:11:53 -0300

    navbar criada

---
+++ src/components/Global/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+    <section class="bg-black text-white h-screen p-5" id="navbar">
+        <div class="flex justify-center">
+            <a href="/"><img src="@@/logoV2.png" alt="Logo da Plataforma"></a>
+        </div>
+        <Line />
+        <div>
+            <ul class="mt-8">
+                <h1 class="text-orange-500 font-bold">Conteúdos Disponíveis</h1>
+                <li class="mt-4 text-2xl text-gray-400">Java</li>
+                <li class="text-2xl text-gray-400">PHP</li>
+                <li class="text-2xl text-gray-400">Spring</li>
+                <a href="/aulasvue">
+                    <li class="text-2xl text-gray-400">Vue Js</li>
+                </a>
+                <a href="/aulaslaravel">
+                    <li class="text-2xl text-gray-400">Laravel</li>
+                </a>
+                <li class="text-2xl text-gray-400">Python</li>
+            </ul>
+        </div>
+        <Line />
+        <Networks />
+    </section>
+</template>
+
+<script>
+import Networks from './Networks.vue';
+import Line from './Line.vue'
+
+export default {
+    name: "Navbar",
+    components: {
+        Networks,
+        Line
+    }
+}
+</script>
+
+<style scoped>
+#navbar {
+    border-right: 3px solid #333;
+    height: 100%;
+}
+
+img {
+    width: 180px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Navbar from "../components/Global/Navbar.vue"
 </script>
 
 <template>
+  <Navbar />
   <main>
-    <TheWelcome />
+    <h1>Olá</h1>
   </main>
 </template>
