15ab52533c9b64bde34e639cf5ccbe9750759a10
Andrew Gomes <andrewgomes1328@gmail.com>
2023-12-18 12:34:33 -0300

    arrumando a página de aula unica

--- src/components/Aula/AulaPorId.vue
+++ src/components/Aula/AulaPorId.vue
@@ -1,43 +1,39 @@
 <template>
-    <section>
-        <h1 class="text-white" v-if="aula">
-            Aula {{ aula.original.id }}
-        </h1>
-        <iframe :src="aula.original.link"
-            title="YouTube video player" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen>
+    <section class="flex p-20">
+      <div v-if="aula">
+        <iframe :src="aula.original.link" title="YouTube video player" frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen>
         </iframe>
+      </div>
     </section>
-</template>
-
-<script>
-
-import axios from 'axios'
-
-export default {
-    name: "AulaPorId",
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  
+  export default {
+    name: "Aula",
     created() {
-        const aulaId = this.$route.params.id;
-
-        const apiUrl = `http://127.0.0.1:8000/api/aulas/${aulaId}`;
-
-        axios.get(apiUrl)
-            .then(response => {
-                this.aula = response.data;
-            })
-            .catch(error => {
-                console.error('Erro ao buscar os detalhes da aula:', error);
-            });
+      const aulaId = this.$route.params.id;
+      const apiUrl = `http://127.0.0.1:8000/api/aulas/${aulaId}`;
+  
+      axios.get(apiUrl)
+        .then(response => {
+          this.aula = response.data;
+        })
+        .catch(error => {
+          console.error('Erro ao buscar os detalhes da aula:', error);
+        });
     },
     data() {
-        return {
-            aula: null
-        };
+      return {
+        aula: null
+      };
     },
-};
-</script>
-
-<style scoped>
-
-</style>
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
--- src/views/Aula.vue
+++ src/views/Aula.vue
@@ -1,12 +1,21 @@
 <template>
-    <AulaPorId />
+    <div class="flex">
+        <div class="w-1/5">
+            <Navbar />
+        </div>
+        <section class="bg-black text-white flex flex-col justify-center items-center p-5 w-4/5">
+            <AulaPorId />
+        </section>
+    </div>
 </template>
 
 <script>
+import Navbar from '../components/Global/Navbar.vue'
 import AulaPorId from '../components/Aula/AulaPorId.vue';
 export default {
     name: "Aula",
     components: {
+        Navbar,
         AulaPorId
     }
 }
